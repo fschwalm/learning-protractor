17343cd41a249d1a662eba0a84eae64d4706d6d5
fagner <fagnerschwalm@gmail.com>
2015-10-22 16:18:36 -0200

    Add jasmineOpts.

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,9 +1,18 @@
 exports.config = {
     multiCapabilities: [{
-        'browserName': 'firefox'
-    }, {
         'browserName': 'chrome'
     }],
     directConnect: true,
-    specs: ['super-calculator/*.js']
+    specs: ['super-calculator/*.js'],
+
+    jasmineNodeOpts: {
+        // If true, display spec names.
+        isVerbose: true,
+        // If true, print colors to the terminal.
+        showColors: true,
+        // If true, include stack traces in failures.
+        includeStackTrace: true,
+        // Default time to wait in ms before a test fails.
+        defaultTimeoutInterval: 30000
+    },
 };
--- test/e2e/super-calculator/my-spec.js
+++ test/e2e/super-calculator/my-spec.js
@@ -19,42 +19,54 @@ describe('Protractor Demo App', function() {
         goButton.click();
 
         expect(latestResult.getText()).toEqual('3');
-    }); it('should add one and two', function() {
+    });
+
+    it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
 
         goButton.click();
 
         expect(latestResult.getText()).toEqual('3');
-    }); it('should add one and two', function() {
+    });
+
+    it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
 
         goButton.click();
 
         expect(latestResult.getText()).toEqual('3');
-    }); it('should add one and two', function() {
+    });
+
+    it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
 
         goButton.click();
 
         expect(latestResult.getText()).toEqual('3');
-    }); it('should add one and two', function() {
+    });
+
+    it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
 
         goButton.click();
 
         expect(latestResult.getText()).toEqual('3');
-    }); it('should add one and two', function() {
+    });
+
+    it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
 
         goButton.click();
 
         expect(latestResult.getText()).toEqual('3');
-    }); it('should add one and two', function() {
+    });
+
+    it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
 
